41e1469d995a8ea82ccef360aa6e1d767e737801
苏鹏飞 <supengfei@supengfeideMacBook-Pro.local>
2018-12-02 11:20:09 +0800

    test http

--- server.js
+++ server.js
@@ -1,47 +1,61 @@
-const express=require('express')
+const http=require('http')
+http.createServer(function (request, response) {
 
-const app = express()
-const mongoose =require("mongoose")
-//const axios =require("axios")
-const DB_URL='mongodb://127.0.0.1:27017'
-mongoose.connect(DB_URL)
-mongoose.connection.on('connected',function(){
-    console.log('mongo is start')
-})
+    // 发送 HTTP 头部 
+    // HTTP 状态值: 200 : OK
+    // 内容类型: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
 
-const User= mongoose.model('user',new mongoose.Schema({
-    user:{type:String,require:true},
-    age:{type:String,require:true}
-}))
+    // 发送响应数据 "Hello World"
+    response.end('Hello World\n');
+}).listen(8888);
+
+// 终端打印如下信息
+console.log('Server running at http://127.0.0.1:8888/');
+// const express=require('express')
+
+// const app = express()
+// const mongoose =require("mongoose")
+// //const axios =require("axios")
+// const DB_URL='mongodb://127.0.0.1:27017'
+// mongoose.connect(DB_URL)
+// mongoose.connection.on('connected',function(){
+//     console.log('mongo is start')
+// })
+
+// const User= mongoose.model('user',new mongoose.Schema({
+//     user:{type:String,require:true},
+//     age:{type:String,require:true}
+// }))
 
 
 
-User.create({
-    user:'soar',
-    age:18
-},function(err,doc){
-    if(err){
-        console.log(err)
-    }else
-    console.log(doc);
-})
+// User.create({
+//     user:'soar',
+//     age:18
+// },function(err,doc){
+//     if(err){
+//         console.log(err)
+//     }else
+//     console.log(doc);
+// })
 
-app.get('/',function(req,res){
-    res.send('<h1>hello world</h1>')
-})
-app.get('set',function(req,res){
-})
-app.get('get',function(req,res){
+// app.get('/',function(req,res){
+    // res.send('<h1>hello world</h1>')
+// })
+// app.get('set',function(req,res){
+// })
+// app.get('get',function(req,res){
 
-    User.find({},function(err,doc){
-        if(!err){
-            res.json(doc)
-        }
-    })
+//     User.find({},function(err,doc){
+//         if(!err){
+//             res.json(doc)
+//         }
+//     })
     
-})
-app.listen(6666,function(){
-    console.log('express is listen 6666');
+// })
+// app.listen(3000,function(){
+    // console.log('express is listen 6666');
 //     setTimeout(function(){
 //         axios.get('http://localhost:6666/get')
 //         .then(res=>{
@@ -50,4 +64,4 @@ app.listen(6666,function(){
 //             console.log(err)
 //         })
 //     },3000)
-})
+// })
